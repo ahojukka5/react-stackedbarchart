506adacb2702a24a6016ddac918c4849a9be60ac
Jukka Aho <ahojukka5@gmail.com>
2020-04-22 00:23:41 +0700

    Placeholder index.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const StackedBarChart = () => {
+  return (
+    <svg>
+      <circle
+        cx="50"
+        cy="50"
+        r="40"
+        stroke="black"
+        stroke-width="3"
+        fill="red"
+      />
+    </svg>
+  );
+};
+
+export default StackedBarChart;
